b4b0bf0a667e77d3d3b1c082cc27f4b94c8e721d
Max Filipe <maxfilipe1@gmail.com>
2019-09-20 18:45:50 -0300

    arrumado paginação e feito tratamento para resposta vazia na pesquisa

--- prova-sambatech/src/app/home/home.component.ts
+++ prova-sambatech/src/app/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
   conteudoDiv = [];
   buscaAtiva = false;
   paginaAtual = 1;
+  semResultados = false;
 
   constructor(private pesquisaService: ApiService) { }
 
@@ -39,6 +40,9 @@ export class HomeComponent implements OnInit {
     console.log(dados);
     this.buscaAtiva = true;
     this.conteudoDiv = dados;
+    if(dados.length === 0){
+      this.semResultados = true;
+    }
   }
 
   proximaPagina(){
@@ -81,6 +85,50 @@ export class HomeComponent implements OnInit {
     }
     this.paginaAtual = this.paginaAtual - 1;
     console.log(this.paginaAtual);
+  }
+
+  pagina1(){
+    this.paginaAtual = 1;
+    this.pesquisaService.pegarPersonagens(0)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  pagina2(){
+    this.paginaAtual = 2;
+    this.pesquisaService.pegarPersonagens(10)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  pagina3(){
+    this.paginaAtual = 3;
+    this.pesquisaService.pegarPersonagens(20)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  pagina4(){
+    this.paginaAtual = 4;
+    this.pesquisaService.pegarPersonagens(30)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  pagina5(){
+    this.paginaAtual = 5;
+    this.pesquisaService.pegarPersonagens(40)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  pagina6(){
+    this.paginaAtual = 6;
+    this.pesquisaService.pegarPersonagens(50)
+    .subscribe(dados => this.preencherDiv(dados));
+  }
+
+  botaoAnterior(){
+    if(this.paginaAtual > 1){
+      return true;
+    }else{
+      return false;
+    }
   }
 
 }
